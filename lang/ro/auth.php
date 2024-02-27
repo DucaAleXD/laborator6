32f30354c6654738d4b462dbdc926c494cf681f4
DucaAleXD <ducasasha@gmail.com>
2024-02-27 10:19:45 +0200

    firts commit

---
+++ lang/ro/auth.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'failed' => 'Datele de autentificare nu corespund înregistrărilor noastre.',
+    'password' => 'Parola furnizată este incorectă.',
+    'throttle' => 'Prea multe încercări de autentificare. Vă rugăm să încercați din nou peste :seconds secunde.',
+    'log_in' => 'Autentificare',
+    'register' => 'Registrare'
+];
+
---
+++ lang/ro/pagination.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'previous' => '&laquo; Precedentul',
+    'next' => 'Următorul &raquo;',
+];
+
